69b1bae10b791a6bc9fbbacf9eef544238be662b
Mighty Mike <nanaadams246@gmail.com>
2022-02-15 04:39:57 +0000

    work completio

--- js/index.js
+++ js/index.js
@@ -10,3 +10,42 @@ function menuToggle() {
     menuItems.style.maxHeight = "0px";
   }
 }
+
+// For Single Product Gallery
+const productImage = document.getElementById("productImg");
+const smallImage = document.getElementsByClassName("small-img");
+
+smallImage[0].onclick = function () {
+  productImage.src = smallImage[0].src;
+};
+smallImage[1].onclick = function () {
+  productImage.src = smallImage[1].src;
+};
+smallImage[2].onclick = function () {
+  productImage.src = smallImage[2].src;
+};
+smallImage[3].onclick = function () {
+  productImage.src = smallImage[3].src;
+};
+
+// For toggle login & register form
+
+function registerFn() {
+  const loginForm = document.getElementById("logForm");
+  const registerForm = document.getElementById("regForm");
+  const indicator = document.getElementById("indicator");
+
+  registerForm.classList.remove("non-active-form");
+  loginForm.classList.add("non-active-form");
+  indicator.style.transform = "translateX(100px)";
+}
+
+function loginFn() {
+  const loginForm = document.getElementById("logForm");
+  const registerForm = document.getElementById("regForm");
+  const indicator = document.getElementById("indicator");
+
+  registerForm.classList.add("non-active-form");
+  loginForm.classList.remove("non-active-form");
+  indicator.style.transform = "translateX(0px)";
+}
